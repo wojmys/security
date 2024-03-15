277fa505853ad5d3c8c74e12dc973bbfabe4055e
Wojciech Mysliwiec <wojciech.mysliwiec@funke-engineering.de>
2024-03-15 08:05:06 +0100

    add User Entity

--- src/main/java/com/example/security/user/User.java
+++ src/main/java/com/example/security/user/User.java
@@ -1,2 +1,24 @@
-package com.example.security.user;public class User {
+package com.example.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="_user")
+public class User
+{
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Integer id;
+   private String firstname;
+   private String lastname;
+   private String email;
+   private String password;
 }
