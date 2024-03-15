cf8e19652dbd38ff675a2f7a91bf444172694133
Wojciech Mysliwiec <wojciech.mysliwiec@funke-engineering.de>
2024-03-15 08:47:17 +0100

    implements UserDetails interface

--- src/main/java/com/example/security/user/User.java
+++ src/main/java/com/example/security/user/User.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @Data
@@ -12,7 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="_user")
-public class User
+public class User implements UserDetails
 {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,4 +27,48 @@ public class User
    private String lastname;
    private String email;
    private String password;
+
+   @Enumerated(EnumType.STRING)
+   private Role role;
+
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities()
+   {
+      return List.of(new SimpleGrantedAuthority(role.name()));
+   }
+
+   @Override public String getPassword()
+   {
+      return password;
+   }
+
+   @Override
+   public String getUsername()
+   {
+      return email;
+   }
+
+   @Override
+   public boolean isAccountNonExpired()
+   {
+      return true;
+   }
+
+   @Override
+   public boolean isAccountNonLocked()
+   {
+      return true;
+   }
+
+   @Override
+   public boolean isCredentialsNonExpired()
+   {
+      return true;
+   }
+
+   @Override
+   public boolean isEnabled()
+   {
+      return true;
+   }
 }
