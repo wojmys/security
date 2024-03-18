87c775088a724899f119256836fa8060202d5aad
Wojciech Mysliwiec <wojciech.mysliwiec@funke-engineering.de>
2024-03-18 09:31:03 +0100

    added DemoController

--- src/main/java/com/example/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/security/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration
 {
    private final JwtAuthentificationFilter jwtAuthFilter;
-   private AuthenticationProvider authenticationProvider;
+   private final AuthenticationProvider authenticationProvider;
 
    @Bean
    @SuppressWarnings( "all" )
