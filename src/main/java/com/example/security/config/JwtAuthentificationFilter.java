570c1356434397cade68436a33818e0e57880272
Wojciech Mysliwiec <wojciech.mysliwiec@funke-engineering.de>
2024-03-18 08:36:39 +0100

    add ApplicationConfig & JwtAuthentificationFilter

--- src/main/java/com/example/security/config/JwtAuthentificationFilter.java
+++ src/main/java/com/example/security/config/JwtAuthentificationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +22,8 @@ public class JwtAuthentificationFilter extends OncePerRequestFilter
 {
 
    private final JwtService jwtService;
+   private final UserDetailsService userDetailsService;
+
    @Override
    protected void doFilterInternal(
 
@@ -29,12 +36,28 @@ public class JwtAuthentificationFilter extends OncePerRequestFilter
       final String authHeader = request.getHeader("Authorization");
       final String jwt;
       final String userEmail;
-      if (authHeader == null || !authHeader.startsWith("Bearer")){
-         filterChain.doFilter(request,response);
+      if (authHeader == null || !authHeader.startsWith("Bearer")) {
+         filterChain.doFilter(request, response);
          return;
       }
       // beginIndex according to "Bearer"
-     jwt= jwt.substring(7);
-      userEmail = jwt.extractUsername(jwt);
+      jwt = authHeader.substring(7);
+      userEmail = jwtService.extractUsername(jwt);
+      if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+         // it means that user is not yet authenticated
+         UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+         if(jwtService.isTokenValid(jwt, userDetails)) {
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                     userDetails,
+                     null,
+                     userDetails.getAuthorities()
+            );
+            authToken.setDetails(
+                     new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+         }
+         filterChain.doFilter(request, response);
+      }
    }
 }
