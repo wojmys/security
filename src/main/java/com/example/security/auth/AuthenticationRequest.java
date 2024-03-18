71e66e5279585d63c1ce040c2586173879605e32
Wojciech Mysliwiec <wojciech.mysliwiec@funke-engineering.de>
2024-03-18 08:33:33 +0100

    add AuthenticationController

---
+++ src/main/java/com/example/security/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.example.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthenticationRequest
+{
+   private String email;
+   private String password;
+}
